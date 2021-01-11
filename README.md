ce8dd27eaa28a46643513ea2850588f417eb6973
Pathak Visham <110142@eesd.org>
2021-01-11 23:57:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+##output
+
+Output is a the way computer prints information to the user in the "Console"
+
+To out we use `print`
+
+#Comments
+
+Comments are notes that the compiler skips over
+
+To comment we use `#`
---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World")
+# This print statement prints out 'Hello World'
+
+# Write 5 print statements
+print("My name is Visham")
+print("I am in 7th Grade")
+print("My favorite color is black")
+print("My favorite sport is basketball")
+print("My favorite ice cream flavor is Mint Chip")
